7ee2a2f037d69c332616216b020eae1b795b1054
Alex M_wrk <mawunka160688@gmail.com>
2017-09-12 12:08:02 +0300

    solved homework1

---
+++ 1st_lection/homework.py
@@ -0,0 +1,12 @@
+def d(a,b,c):
+	dis = b**2-4*a*c
+	if dis < 0:
+		return '??? ?????????????? ??????'
+	if dis == 0:
+		x = -b/2*a
+		return round(x, 3)
+	else:
+		dis = dis**0.5
+		x1 = (-b+dis)/(2*a)
+		x2 = (-b-dis)/(2*a)
+		return round(x1,3), round(x2,3)
